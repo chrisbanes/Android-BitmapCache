c32ce4025a23ecba8988652ae49ea31c4275efcc
Chris Banes <chris@senab.co.uk>
2012-07-01 09:16:26 +0100

    Basic code. Needs refining and doc'ing

---
+++ src/uk/co/senab/bitmapcache/CacheableImageView.java
@@ -0,0 +1,76 @@
+package uk.co.senab.bitmapcache;
+
+import uk.co.senab.bitmapcache.cache.CacheableBitmapWrapper;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class CacheableImageView extends ImageView {
+
+	private CacheableBitmapWrapper mDisplayedBitmapWrapper;
+
+	public CacheableImageView(Context context) {
+		super(context);
+	}
+
+	public CacheableImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	/**
+	 * 
+	 * @param wrapper
+	 */
+	public void setImageCachedBitmap(final CacheableBitmapWrapper wrapper) {
+		if (null != wrapper) {
+			wrapper.setDisplayed(true);
+			setImageBitmap(wrapper.getBitmap());
+		} else {
+			setImageDrawable(null);
+		}
+
+		// Finally, set our new BitmapWrapper
+		mDisplayedBitmapWrapper = wrapper;
+	}
+
+	@Override
+	public void setImageBitmap(Bitmap bm) {
+		BitmapDrawable d = new BitmapDrawable(getResources(), bm);
+		setImageDrawable(d);
+	}
+
+	@Override
+	public void setImageDrawable(Drawable drawable) {
+		super.setImageDrawable(drawable);
+		resetCachedDrawable();
+	}
+
+	@Override
+	public void setImageResource(int resId) {
+		super.setImageResource(resId);
+		resetCachedDrawable();
+	}
+
+	@Override
+	protected void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+
+		// Will cause displayed bitmap wrapper to be 'free-able'
+		setImageDrawable(null);
+	}
+
+	/**
+	 * Called when the current cached bitmap has been removed. This method will
+	 * remove the displayed flag and remove this objects reference to it.
+	 */
+	private void resetCachedDrawable() {
+		if (null != mDisplayedBitmapWrapper) {
+			mDisplayedBitmapWrapper.setDisplayed(false);
+			mDisplayedBitmapWrapper = null;
+		}
+	}
+
+}
---
+++ src/uk/co/senab/bitmapcache/cache/BitmapLruCache.java
@@ -0,0 +1,90 @@
+package uk.co.senab.bitmapcache.cache;
+
+import java.util.Map.Entry;
+
+import uk.co.senab.bitmapcache.BuildConfig;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+import android.util.Log;
+
+public class BitmapLruCache extends LruCache<String, CacheableBitmapWrapper> {
+
+	static final int DEFAULT_CACHE_SIZE = 8; // 1/8th
+	static final String LOG_TAG = "BitmapLruCache";
+	static final int MEGABYTE = 1024 * 1024;
+
+	/**
+	 * Initialise LruCache with default size of 1/8th of heap size.
+	 * 
+	 * @param context
+	 */
+	public BitmapLruCache(Context context) {
+		this(MEGABYTE
+				* ((ActivityManager) context
+						.getSystemService(Context.ACTIVITY_SERVICE))
+						.getMemoryClass() / DEFAULT_CACHE_SIZE);
+	}
+
+	public BitmapLruCache(int maxSize) {
+		super(maxSize);
+	}
+
+	@Override
+	protected int sizeOf(String key, CacheableBitmapWrapper value) {
+		if (value.hasValidBitmap()) {
+			Bitmap bitmap = value.getBitmap();
+			return bitmap.getRowBytes() * bitmap.getHeight();
+		}
+		return 0;
+	}
+
+	/**
+	 * Convenience method that just calls through to
+	 * {@link LruCache#put(K key, V value) } with the Key as
+	 * {@link CacheableBitmapWrapper#getUrl() }
+	 * 
+	 * @param newValue
+	 *            - Value to put into cache
+	 * @return oldValue
+	 */
+	public CacheableBitmapWrapper put(CacheableBitmapWrapper newValue) {
+		return put(newValue.getUrl(), newValue);
+	}
+
+	@Override
+	protected void entryRemoved(boolean evicted, String key,
+			CacheableBitmapWrapper oldValue, CacheableBitmapWrapper newValue) {
+
+		if (BuildConfig.DEBUG) {
+			Log.d(LOG_TAG, "EntryRemoved. Key: " + key);
+		}
+
+		// If the value being removed isn't being displayed, recycle it
+		if (!oldValue.isBeingDisplayed()) {
+			Bitmap bitmap = oldValue.getBitmap();
+			bitmap.recycle();
+		} else {
+			// Should handle here. Maybe a second-level SoftReference Cache?
+		}
+	}
+
+	/**
+	 * This method iterates through the cache and removes any Bitmap entries
+	 * which are not currently being displayed. A good place to call this would
+	 * be from {@link android.app.Application#onLowMemory()
+	 * Application.onLowMemory()}.
+	 */
+	public void trimMemory() {
+		for (Entry<String, CacheableBitmapWrapper> entry : snapshot()
+				.entrySet()) {
+			CacheableBitmapWrapper value = entry.getValue();
+			if (null == value || !value.isBeingDisplayed()) {
+				remove(entry.getKey());
+			}
+		}
+	}
+
+}
