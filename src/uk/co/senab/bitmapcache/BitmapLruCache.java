4877cbacf0dfdb1caded624d68959c99be588028
Chris Banes <chris@senab.co.uk>
2012-07-11 09:31:38 +0100

    Allow configurable size of cache based on heap size

--- src/uk/co/senab/bitmapcache/BitmapLruCache.java
+++ src/uk/co/senab/bitmapcache/BitmapLruCache.java
@@ -9,20 +9,33 @@ import android.graphics.Bitmap;
 
 public class BitmapLruCache extends LruCache<String, CacheableBitmapWrapper> {
 
-	static final int DEFAULT_CACHE_SIZE = 8; // 1/8th
+	static final float DEFAULT_CACHE_SIZE = 1f / 8f;
+	static final float MAX_CACHE_SIZE = 1f;
+
 	static final String LOG_TAG = "BitmapLruCache";
 	static final int MEGABYTE = 1024 * 1024;
 
 	/**
-	 * Initialise LruCache with default size of 1/8th of heap size.
+	 * Initialise LruCache with default size of {@value #DEFAULT_CACHE_SIZE} of heap size.
 	 * 
-	 * @param context
+	 * @param context - context
 	 */
 	public BitmapLruCache(Context context) {
-		this(MEGABYTE
-				* ((ActivityManager) context
-						.getSystemService(Context.ACTIVITY_SERVICE))
-						.getMemoryClass() / DEFAULT_CACHE_SIZE);
+		this(context, DEFAULT_CACHE_SIZE);
+	}
+
+	/**
+	 * Initialise LruCache with the given percentage of heap size. This is
+	 * capped at {@value #MAX_CACHE_SIZE} denoting 100% of the app heap size.
+	 * 
+	 * @param context
+	 *            - Context
+	 * @param percentageOfHeap
+	 *            - percentage of heap size. Valid values are 0.0 <= x <= 1.0.
+	 */
+	public BitmapLruCache(Context context, float percentageOfHeap) {
+		this(Math.round(MEGABYTE * getHeapSize(context)
+				* Math.min(percentageOfHeap, MAX_CACHE_SIZE)));
 	}
 
 	public BitmapLruCache(int maxSize) {
@@ -50,7 +63,7 @@ public class BitmapLruCache extends LruCache<String, CacheableBitmapWrapper> {
 	public CacheableBitmapWrapper put(CacheableBitmapWrapper newValue) {
 		return put(newValue.getUrl(), newValue);
 	}
-	
+
 	@Override
 	public CacheableBitmapWrapper put(String key, CacheableBitmapWrapper value) {
 		// Notify the wrapper that it's being cached
@@ -81,4 +94,8 @@ public class BitmapLruCache extends LruCache<String, CacheableBitmapWrapper> {
 		}
 	}
 
+	private static int getHeapSize(Context context) {
+		return ((ActivityManager) context
+				.getSystemService(Context.ACTIVITY_SERVICE)).getMemoryClass();
+	}
 }
