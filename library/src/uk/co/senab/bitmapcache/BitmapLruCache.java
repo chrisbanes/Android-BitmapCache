8849b8877f07635d5627411ff0fbe552571f9954
Chris Banes <chris@senab.co.uk>
2012-12-17 13:50:28 +0000

    Fix Copyrights

--- library/src/uk/co/senab/bitmapcache/BitmapLruCache.java
+++ library/src/uk/co/senab/bitmapcache/BitmapLruCache.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2011, 2012 Chris Banes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package uk.co.senab.bitmapcache;
 
 import java.io.File;
@@ -288,10 +303,12 @@ public class BitmapLruCache {
 	}
 
 	private void scheduleDiskCacheFlush() {
+		// If we already have a flush scheduled, cancel it
 		if (null != mDiskCacheFuture) {
 			mDiskCacheFuture.cancel(false);
 		}
 
+		// Schedule a flush
 		mDiskCacheFuture = mDiskCacheFlusherExecutor.schedule(mDiskCacheFlusherRunnable, DISK_CACHE_FLUSH_DELAY_SECS,
 				TimeUnit.SECONDS);
 	}
