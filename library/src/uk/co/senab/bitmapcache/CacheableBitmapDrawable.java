chrisbanes/Android-BitmapCache

commit 35cd4fbc223c8dca9cde7e53eda1c15571bc8104

A more clever solid implementation to fix the race condition found in 463c1c14ab79e82578b48d371e7e1f194c5e4f19. We now delay a checkState() call if find the condition to occur.

library/src/uk/co/senab/bitmapcache/CacheableBitmapDrawable.java
@@ -2,6 +2,8 @@ package uk.co.senab.bitmapcache;
 
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 public class CacheableBitmapDrawable extends BitmapDrawable {
@@ -18,6 +20,9 @@ public class CacheableBitmapDrawable extends BitmapDrawable {
 
 	// Number of caches currently referencing the wrapper
 	private int mCacheCount;
+
+	// Handler which may be used later
+	private static final Handler sHandler = new Handler(Looper.getMainLooper());
 
 	public CacheableBitmapDrawable(Bitmap bitmap) {
 		this(null, bitmap);
@@ -114,23 +119,72 @@ public class CacheableBitmapDrawable extends BitmapDrawable {
 	}
 
 	/**
-	 * Checks whether the wrapper is currently referenced, and is being
-	 * displayed. If neither of those conditions are met then the bitmap is
-	 * recycled and freed.
+	 * Calls {@link #checkState(boolean)} with default parameter of
+	 * <code>false</code>.
 	 */
 	private void checkState() {
+		checkState(false);
+	}
+
+	/**
+	 * Checks whether the wrapper is currently referenced by a cache, and is
+	 * being displayed. If neither of those conditions are met then the bitmap
+	 * is ready to be recycled. Whether this happens now, or is delayed depends
+	 * on whether the Drawable has been displayed or not.
+	 * <ul>
+	 * <li>If it has been displayed, it is recycled straight away.</li>
+	 * <li>If it has not been displayed, and <code>ignoreBeenDisplayed</code> is
+	 * <code>false</code>, a call to <code>checkState(true)</code> is queued to
+	 * be called after a delay.</li>
+	 * <li>If it has not been displayed, and <code>ignoreBeenDisplayed</code> is
+	 * <code>true</code>, it is recycled straight away.</li>
+	 * </ul>
+	 * 
+	 * @see Constants#UNUSED_DRAWABLE_RECYCLE_DELAY_MS
+	 * 
+	 * @param ignoreBeenDisplayed - Whether to ignore the 'has been displayed'
+	 *            flag when deciding whether to recycle() now.
+	 */
+	private synchronized void checkState(final boolean ignoreBeenDisplayed) {
 		if (Constants.DEBUG) {
 			Log.d(LOG_TAG, String.format("checkState(). Been Displayed: %b, Displaying: %d, Caching: %d, URL: %s",
 					mHasBeenDisplayed, mDisplayingCount, mCacheCount, mUrl));
 		}
 
 		// We only want to recycle if it has been displayed.
-		if (mHasBeenDisplayed && mCacheCount <= 0 && mDisplayingCount <= 0 && hasValidBitmap()) {
-			if (Constants.DEBUG) {
-				Log.d(LOG_TAG, "Recycling bitmap with url: " + mUrl);
+		if (mCacheCount <= 0 && mDisplayingCount <= 0 && hasValidBitmap()) {
+
+			/**
+			 * If we have been displayed or we don't care whether we have been
+			 * or not, then recycle() now. Otherwise, we retry in 1 second.
+			 */
+			if (mHasBeenDisplayed || ignoreBeenDisplayed) {
+				if (Constants.DEBUG) {
+					Log.d(LOG_TAG, "Recycling bitmap with url: " + mUrl);
+				}
+				getBitmap().recycle();
+			} else {
+				if (Constants.DEBUG) {
+					Log.d(LOG_TAG, "Unused Bitmap which hasn't been displayed, delaying recycle(): " + mUrl);
+				}
+				sHandler.postDelayed(new CheckStateRunnable(), Constants.UNUSED_DRAWABLE_RECYCLE_DELAY_MS);
 			}
-			getBitmap().recycle();
 		}
+	}
+
+	/**
+	 * Runnable which run a {@link CacheableBitmapDrawable#checkState(boolean)
+	 * checkState(false)} call.
+	 * 
+	 * @author chrisbanes
+	 */
+	private final class CheckStateRunnable implements Runnable {
+
+		@Override
+		public void run() {
+			checkState(true);
+		}
+
 	}
 
 }

library/src/uk/co/senab/bitmapcache/Constants.java
@@ -21,4 +21,6 @@ class Constants {
 
 	static String LOG_TAG = "BitmapCache";
 
+	static final int UNUSED_DRAWABLE_RECYCLE_DELAY_MS = 1000;
+
 }

commit 0990180c3966537b7f9343e8604f16ab5278aee1

Fix possible memory leak caused in 35cd4fbc223c8dca9cde7e53eda1c15571bc8104

library/src/uk/co/senab/bitmapcache/CacheableBitmapDrawable.java
@@ -167,7 +167,7 @@ public class CacheableBitmapDrawable extends BitmapDrawable {
 				if (Constants.DEBUG) {
 					Log.d(LOG_TAG, "Unused Bitmap which hasn't been displayed, delaying recycle(): " + mUrl);
 				}
-				sHandler.postDelayed(new CheckStateRunnable(), Constants.UNUSED_DRAWABLE_RECYCLE_DELAY_MS);
+				sHandler.postDelayed(new CheckStateRunnable(this), Constants.UNUSED_DRAWABLE_RECYCLE_DELAY_MS);
 			}
 		}
 	}
@@ -178,13 +178,16 @@ public class CacheableBitmapDrawable extends BitmapDrawable {
 	 * 
 	 * @author chrisbanes
 	 */
-	private final class CheckStateRunnable implements Runnable {
+	private static final class CheckStateRunnable extends WeakReferenceRunnable<CacheableBitmapDrawable> {
 
-		@Override
-		public void run() {
-			checkState(true);
+		public CheckStateRunnable(CacheableBitmapDrawable object) {
+			super(object);
 		}
 
+		@Override
+		public void run(CacheableBitmapDrawable object) {
+			object.checkState(true);
+		}
 	}
 
 }

library/src/uk/co/senab/bitmapcache/WeakReferenceRunnable.java
@@ -1,0 +1,24 @@
+package uk.co.senab.bitmapcache;
+
+import java.lang.ref.WeakReference;
+
+abstract class WeakReferenceRunnable<T> implements Runnable {
+
+	private final WeakReference<T> mObjectRef;
+
+	public WeakReferenceRunnable(T object) {
+		mObjectRef = new WeakReference<T>(object);
+	}
+
+	@Override
+	public final void run() {
+		T object = mObjectRef.get();
+
+		if (null != object) {
+			run(object);
+		}
+	}
+
+	public abstract void run(T object);
+
+}